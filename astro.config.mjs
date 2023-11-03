0aa370b9bb0a5ebf834fdccf073da7743b64ff56
jesper <jesholmlund@gmail.com>
2023-11-03 12:05:05 +0100

    adding netlifty

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
 import fuse from "astro-fuse";
+import netlify from "@astrojs/netlify/functions";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), fuse()],
   output: "hybrid",
+  adapter: netlify()
 });
