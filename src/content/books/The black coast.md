b3f82558a3938844c25e8d6070e540eb6d025c17
jeshol <jesholmlund@gmail.com>
2024-01-15 21:29:20 +0100

    design change

---
+++ src/content/books/The black coast.md
@@ -0,0 +1,13 @@
+---
+title: The black coast
+tags: [ett, två, tre]
+author: Mike Brooks
+authorSlug: mike-brooks
+pages: 672
+rating: 4
+image: https://image.bokus.com/images/9780356513911_200x_the-black-coast_haftad
+excerpt: When the citizens of Black Keep see ships on the horizon, terror takes them because they know who is coming for generations, the keep has been raided by the fearsome clanspeople of Tjakorsha. Saddling their war dragons, Black Keep's warriors rush to defend their home only to discover that the clanspeople have not come to pillage at all. Driven from their own land by a daemonic despot who prophesises the end of the world, the raiders come in search of a new home . . . Meanwhile the wider continent of Narida is lurching toward war. Black Keep is about to be caught in the crossfire - if only its new mismatched society can survive
+publishDate: 2024-01-10
+---
+
+When the citizens of Black Keep see ships on the horizon, terror takes them because they know who is coming: for generations, the keep has been raided by the fearsome clanspeople of Tjakorsha. Saddling their war dragons, Black Keep's warriors rush to defend their home only to discover that the clanspeople have not come to pillage at all. Driven from their own land by a daemonic despot who prophesises the end of the world, the raiders come in search of a new home . . . Meanwhile the wider continent of Narida is lurching toward war. Black Keep is about to be caught in the crossfire - if only its new mismatched society can survive
--- src/pages/api/books.js
+++ src/pages/api/books.js
@@ -1,25 +1,25 @@
 import { getCollection } from "astro:content";
-const books = await getCollection("books")
+const books = await getCollection("books");
 export const prerender = false;
 
 export async function POST({ params, request }) {
-    const data = await request.formData()
-    const query = data.get("search")
-    const sorted = books.filter(book => book.data.title.includes(query))
-    return new Response(sorted.map((book)=>{
-         return `<article class=" p-4  bg-slate-50 rounded-2xl ">
+  const data = await request.formData();
+  const query = data.get("search");
+  const sorted = books.filter((book) => book.data.title.includes(query));
+  return new Response(
+    sorted
+      .map((book) => {
+        return `<article class=" rounded-2xl ">
          <div class=" text-black text-2xl font-medium">
          <h5 transition:name="title-${book.slug}"><a href=/library/${book.slug} class="hover:text-slate-700">${book.data.title}</a></h5>
          </div>
          <div class=" flex flex-col justify-center gap-8 items-start entry-content">
              <div class="text-md excerpt line-clamp-3">
-                   Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quia expedita doloremque repellendus voluptate unde illum nulla. 
-                   Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quia expedita doloremque repellendus voluptate unde illum nulla.
-                   Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quia expedita doloremque repellendus voluptate unde illum nulla.
-                   Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quia expedita doloremque repellendus voluptate unde illum nulla.
+                  ${book.data.excerpt}
              </div>
          </div>
-     </article>`
-    }).join(" ")
-    )
-  }
+     </article>`;
+      })
+      .join(" ")
+  );
+}
